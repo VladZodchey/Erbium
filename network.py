1743b8f3e533806d9978ac1b565ffc0830c2df6b
VladZodchey <igor.nerdov@gmail.com>
2023-10-13 18:47:27 +0300

    Basic network

--- network.py
+++ network.py
@@ -1,1 +1,18 @@
 import socket
+
+HOST = socket.gethostname()
+PORT = 65432
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+    client_socket.connect((HOST, PORT))
+
+    while True:
+        message = input("Write your message: ")
+        client_socket.sendall(message.encode())
+
+        data = client_socket.recv(1024)
+        print('Received', repr(data.decode()))
+
+        if message.lower() == "exit":
+            break
+
