5b9d705dcc46b21aac2f29f98f0c0edf9b205de2
VladZodchey <igor.nerdov@gmail.com>
2023-10-13 22:15:16 +0300

    Literally destroyed everything

--- client.py
+++ client.py
@@ -1,1 +1,41 @@
+import configparser
 import kivy
+import socket
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.core.window import Window
+Window.size = (1280, 100)
+
+cl.soc
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+color1 = config['Appearance']['base']
+color2 = config['Appearance']['second']
+
+class Layout(GridLayout):
+    def __init__(self, **kwargs):
+        super(Layout, self).__init__(**kwargs)
+
+        self.cols = 2
+        self.msg = TextInput(multiline=False)
+        self.add_widget(self.msg)
+
+        self.submit = Button(text='Send!')
+        self.submit.bind(on_press=self.send)
+        self.add_widget(self.submit)
+    def send(self, instance):
+        msg = self.msg.text
+        self.msg.text = ''
+        if msg != '':
+            print(msg)
+        
+class Erbi(App):
+    def build(self):
+        return Layout()
+Erbi().run()
--- network.py
+++ network.py
@@ -1,18 +1,48 @@
 import socket
+import threading
+import kivy
+from kivy.app import App
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
 
 HOST = socket.gethostname()
-PORT = 65432
+PORT = 3245
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
-    client_socket.connect((HOST, PORT))
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((HOST, PORT))
 
+class Layout(GridLayout):
+    def __init__(self, **kwargs):
+        self.cols = 2
+        send = Button(text='Send!')
+        send.bind(on_press=send_message)
+        self.add_widget(self.send)
+        msg = TextInput(multiline=False)
+        self.add_widget(self.msg)
+    def send_message(self, instance):
+        try:
+            message = self.msg.text
+            print(message)
+            client_socket.send(message.encode("utf-8"))
+            self.msg.text = ''
+        except Exception as e:
+            print(f"Error sending data to the server: {str(e)}")
+
+class Erbi(App):
+    def build(self):
+        return Layout()
+
+def receive_message():
     while True:
-        message = input("Write your message: ")
-        client_socket.sendall(message.encode())
-
-        data = client_socket.recv(1024)
-        print('Received', repr(data.decode()))
-
-        if message.lower() == "exit":
+        try:
+            data = client_socket.recv(1024).decode("utf-8")
+            if not data:
+                break
+            print(f"Received from server: {data}")
+        except Exception as e:
+            print(f"Error receiving data from the server: {str(e)}")
             break
 
+receive_thread = threading.Thread(target=receive_message)
+receive_thread.start()
